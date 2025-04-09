428a942998eb43128fdea5bfdb92b2c235429efc
Hans-Yin <tftzseckill@gmail.com>
2025-04-09 09:14:51 -0400

    Frontend allows photos upload, add flask service and upload api

---
+++ web-app/backend/run.py
@@ -0,0 +1,8 @@
+from app import create_app
+import sys
+
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
