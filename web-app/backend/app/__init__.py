cbf1cc424070b84894fd33f67d57126a7a78a68d
Exiam6 <zz4330@nyu.edu>
2025-04-10 11:51:53 -0400

    update docker

--- web-app/backend/app/__init__.py
+++ web-app/backend/app/__init__.py
@@ -15,6 +15,7 @@ def create_app():
         Flask: The configured Flask application.
     """
     app = Flask(__name__)
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     CORS(app, origins=["http://localhost:3000"])
     app.register_blueprint(routes)
     return app
--- web-app/backend/run.py
+++ web-app/backend/run.py
@@ -1,18 +1,11 @@
 """Entry point to run the Flask backend app."""
-from flask import Flask
-from flask_cors import CORS
+
 import sys
+from app import create_app
 
-
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app)  # <- This is important if frontend and backend aren't served from same origin
-    ...
-    return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5114
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     app.run(host="0.0.0.0", port=port, debug=True)
