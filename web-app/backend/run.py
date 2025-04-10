b2b19d673998b1dacf7c3a6aa792be184894a340
Zifan <zz4330@nyu.edu>
2025-04-10 01:53:54 -0400

    refactor detector

--- web-app/backend/run.py
+++ web-app/backend/run.py
@@ -7,5 +7,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5111
     app.run(host="0.0.0.0", port=port, debug=True)
