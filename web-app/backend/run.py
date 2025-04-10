8bbc87e49e6f9433fd494b21a965b8a58cf350e5
Exiam6 <zz4330@nyu.edu>
2025-04-10 12:50:19 -0400

    blacked

--- web-app/backend/run.py
+++ web-app/backend/run.py
@@ -1,4 +1,5 @@
 """Entry point to run the Flask backend app."""
+
 import sys
 from app import create_app
 
@@ -7,4 +8,3 @@ app = create_app()
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5114
     app.run(host="0.0.0.0", port=port, debug=True)
-    
